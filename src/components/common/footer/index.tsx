84f9d44d3eeda10f9a410dcb809e6f4d316afe38
unknown <irfanfathoni73@gmail.com>
2022-08-04 21:43:59 +0700

    Footer with charkra-ui

--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
-import './Footer.css';
+import { Box, Center, Text, Circle, VStack, HStack} from '@chakra-ui/react';
 
 const Footer = () => {
   return (
-    <div className='Footer'>
-      <div className='LogoLayout'>
-        <div className='Logo'></div>
-        <div className='KatItb'>
-          <li>KAT</li>
-          <li>ITB</li>
-          <li>2022</li>
-        </div>
-      </div>
-      <div className='ContentLayout'>
-        Footer Content
-      </div>
-    </div>
+    <Box
+      bg='#ffa06e'
+      p={{ base: '32px', md: '32px', lg: '50px' }}
+    >
+      <VStack spacing={{ base: '0px', md: '0px', lg: '43px' }} mt={{ base: '0px', md: '0px', lg: '20px' }}>
+        <HStack spacing={{ base: '17px', md: '17px', lg: '30px' }}>
+          <Circle size={{ base: '91px', md: '91px', lg: '186px' }} bg='#D9D9D9'></Circle>
+          <Box
+            fontFamily='Alegreya Semibold'
+            fontSize={{ base: '36px', md: '36px', lg: '73px' }}
+            lineHeight={{ base: '33.12px', md: '33.12px', lg: '67.16px' }}
+          >
+            <Text>KAT</Text>
+            <Text>ITB</Text>
+            <Text>2022</Text>
+          </Box>
+        </HStack>
+        <Center bg='#D9D9D9' h={[209, 209, 229]} w={[0, 0, 0, 1320]}>
+          <Text
+            fontFamily='Alegreya Semibold'
+            fontSize={{ base: '0px', md: '0px', lg: '73px' }}
+            lineHeight={{ base: '33.12px', md: '33.12px', lg: '67.16px' }}
+          >
+            Footer Content
+          </Text>
+        </Center>
+      </VStack>
+    </Box>
   );
 };
 
