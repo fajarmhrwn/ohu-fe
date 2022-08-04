4da5c75682f6de96916ebdb623c51b079bdcb302
dyophine7 <16521248@mahasiswa.itb.ac.id>
2022-08-05 00:10:27 +0700

    feat: Page Not Found
    - gambar masih ngambil dari ss-an
    - link back to home masih dummy
    - aku nyoba, font di button ga bisa pake alegreya, masih gatau kenapa :"

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Button,
+  Image,
+  Heading
+} from "@chakra-ui/react"
+import theme from "@theme/theme"
 
 export const PageNotFound = () => {
   return (
-    <div>
-      <h1>Page Not Found</h1>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Box display= "flex" flexDirection="column" alignItems="center" >
+        <Box mt={{base:"46px", md:"0px"}} >
+          <Image objectFit="cover" src={require("@assets/Prisma nungguin sprout.png")} h={{base:"413px", md:"648px"}}/>
+        </Box>
+        <Box mt={{base:"10px", md:"20px"}}>
+          <Heading>
+            <Box fontFamily="Magilio" fontWeight="400" fontSize={{base:"42px", md:"96px"}} color="#231A51" lineHeight="92%" textAlign="center" pr= "50px" pl= "50px">
+              Halaman Tidak Ditemukan
+            </Box>
+          </Heading>
+        </Box>
+        <Box mt={{base:"30px", md:"110px"}} mb={{base:"168px", md:"205px"}}>
+          <Button as="a" href="https://www.google.com" width={{base:"245px", md:"533px"}} height={{base:"40px", md:"69px"}} background="#FFA06F" borderRadius="50px" fontFamily= 'Alegreya Sans Regular' fontWeight="500" fontSize={{base:"18px", md:"40px"}} line-height="92%">
+            Kembali ke halaman utama
+          </Button>
+        </Box>
+      </Box>
+    </ChakraProvider>
   );
 };
--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -5,6 +5,13 @@ const theme = extendTheme({
     heading: `'Magilio', 'sans-serif'`,
     body: `'Alegreya Sans Regular', 'sans-serif'`,
   },
+  styles: {
+    global: () => ({
+      body: {
+        bg: "#FFFCF2"
+      }
+    })
+  },
 });
 
 export default theme;
