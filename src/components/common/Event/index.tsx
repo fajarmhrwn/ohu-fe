93d081d857f021da14616acc6105fa9c671cb186
joli <auliameydivaannandya@Aulias-MacBook-Pro.local>
2022-08-08 01:03:00 +0700

    revisi events

--- src/components/common/Event/index.tsx
+++ src/components/common/Event/index.tsx
@@ -5,35 +5,91 @@ import { Box,
         Image,
         Flex,
         Stack,
-        Button } from '@chakra-ui/react'
+        Button,
+        useMediaQuery,
+        Show,
+        Hide } from '@chakra-ui/react'
 
-
+        
 export const Event: () => JSX.Element = () => {
+const MobileApp = useMediaQuery('(max-width: 640px)')
     return (
-      <Box backgroundColor='#FFEBB0' w='2100px' h='1200px'>
-        <Flex>
-        <Text fontSize='43px' pos='absolute' left='222px' top='360px' w='855px' h='40px'>
-            Apa saja yang dapat kita lakukan di OHU Event?
-        </Text>
-        <Heading fontSize='124px' pos='absolute' left='1520px' top='170px' w='370px' h='136px'>
+            <Box backgroundColor='#FFEBB0'
+            w={{lg: '1920px', base: '640px'}}
+            h='1200'>
+            <Flex>
+            <Heading fontSize={{lg: '124px', base:'70px'}}
+                pos='relative'
+                marginLeft={{lg: '1500px', base:'300px'}} 
+                marginRight='auto'
+                top={{lg: '200px', base:'400px'}}
+                w='370px'
+                h='136px'>
             Events
-        </Heading> 
-        </Flex>
-        <Flex>
-        <Image pos='absolute' top='500px' left='100px' w='700px' src='https://i.postimg.cc/NGPR6LNw/Group-5479.png'/>
-        <Stack direction='column' spacing={4} pos='absolute' top='550px' right='600px' m={10}>
-            <Button backgroundColor='#FFA06F' w='400px' h='70px' borderRadius='50px' fontSize='35px'>
-                OHU Map Tour
-            </Button>
-            <Button backgroundColor='#FFA06F' w='400px' h='70px' borderRadius='50px' fontSize='35px'>
-                Missions
-            </Button>
-            <Button backgroundColor='#FFA06F' w='400px' h='70px' borderRadius='50px' fontSize='35px'>
-                Leaderboards
-            </Button>  
-        </Stack>
-        <Image pos='absolute' w='454px' top='500px' left='1400px' src='https://i.postimg.cc/W3N0nZLs/21-22-joget-2-1.png'/>
-        </Flex>
-        </Box>
+            </Heading> 
+            </Flex>
+            <Hide below='640px'>
+                <Flex >
+                    <Text fontSize='43px'
+                        display={{lg: 'block', base:'none'}} 
+                        pos='absolute' 
+                        left='222px' 
+                        top='360px' 
+                        w='855px' 
+                        h='40px'
+                        >
+                        Apa saja yang dapat kita lakukan di OHU Event?
+                    </Text>
+                </Flex>
+                <Flex >
+                    <Image pos='absolute'
+                        display={{lg: 'block', base:'none'}} 
+                        top='500px' 
+                        marginRight='auto' 
+                        marginLeft='100px' 
+                        w='700px' 
+                        src='https://i.postimg.cc/NGPR6LNw/Group-5479.png'/>
+                </Flex>
+            </Hide>
+            <Flex>
+                <Stack direction='column' 
+                    pos='relative'
+                    top={{lg: '450px', base:'600px'}}
+                    left={{lg: '850px', base:'100px'}}
+                    spacing = {8}
+                    
+                    >
+                    <Button backgroundColor='#FFA06F' 
+                        w='400px' 
+                        h='70px' 
+                        borderRadius='50px'
+                        fontSize='35px'>
+                        OHU Map Tour
+                    </Button>
+                    <Button backgroundColor='#FFA06F' 
+                        w='400px' 
+                        h='70px' 
+                        borderRadius='50px' 
+                        fontSize='35px'>
+                        Missions
+                    </Button>
+                    <Button backgroundColor='#FFA06F' 
+                        w='400px' 
+                        h='70px' 
+                        borderRadius='50px' 
+                        fontSize='35px'>
+                        Leaderboards
+                    </Button>  
+                </Stack>
+            </Flex>
+            <Flex>
+                <Image pos='absolute' 
+                    w={{lg: '454px', base:'300px'}} 
+                    top={{lg: '500px', base:'450px'}} 
+                    marginRight='auto' 
+                    marginLeft={{lg: '1400px', base:'150px'}}
+                    src='https://i.postimg.cc/W3N0nZLs/21-22-joget-2-1.png'/>
+            </Flex>
+            </Box>
     )
 }
