37a449fea9434353de9d4b80d094216f8a5cfeec
raniakiranaa <16521042@std.stei.itb.ac.id>
2022-08-07 20:02:00 +0700

    tambahin navbar dikit

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -1,19 +1,6 @@
 import { useState } from "react";
 import Logo from "../../../assets/react.svg";
-import {
-  Box,
-  Button,
-  Flex,
-  HStack,
-  IconButton,
-  Image,
-  Link,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Button, Flex, HStack, IconButton, Image, Link, Menu, MenuButton, MenuItem, MenuList, Text } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon, ChevronDownIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
@@ -23,29 +10,22 @@ const Navbar = () => {
   return (
     <Flex>
       <Flex
-        width="100%"
-        height="80px"
-        backgroundColor="white"
-        alignItems="center"
-        justify={"space-between"}
-        px={{ md: 20, base: 5 }}
+        width="1770px" height="73px" left="78px" top="109px" alignItems="center" justify={"space-between"} px={{ md: 20, base: 5 }}
       >
         {/* Desktop */}
 
         {/* Logo */}
         <Box alignItems={"center"}>
           <HStack>
-            <Image src={Logo} />
-            <Text fontWeight="extrabold" fontSize="xl">
+            <Image src={Logo}/>
+            <Text fontWeight="400" fontSize="30px">
               KAT ITB 2022
             </Text>
           </HStack>
         </Box>
         {/* Menu Bar */}
         <Flex
-          display={["none", "none", "flex", "flex"]}
-          alignItems="center"
-          gap={20}
+          display={["none", "none", "flex", "flex"]} alignItems="center" gap={20}
         >
           <Flex gap={20}>
             <Link>Home</Link>
@@ -56,10 +36,7 @@ const Navbar = () => {
           {isLogin ? (
             <Menu>
               <MenuButton
-                as={Button}
-                rightIcon={<ChevronDownIcon />}
-                variant="ghost"
-                cursor="pointer"
+                as={Button} rightIcon={<ChevronDownIcon />} variant="ghost" cursor="pointer"
               >
                 Lorem Ipsum
               </MenuButton>
@@ -70,12 +47,7 @@ const Navbar = () => {
             </Menu>
           ) : (
             <Button
-              boxShadow="md"
-              h="30px"
-              fontSize="15px"
-              px={8}
-              color="white"
-              colorScheme="yellow"
+              boxShadow="0px 1px 7px 2px rgba(244, 166, 65, 0.62)" borderRadius="10px" fontSize="15px" h="30px" px={8} color="white" bgColor="#F4A641"
               onClick={() => setIsLogin(true)}
             >
               Log In
@@ -85,38 +57,17 @@ const Navbar = () => {
 
         {/* Mobile */}
         <IconButton
-          aria-label="Open Menu"
-          size="lg"
-          mr={2}
-          icon={<HamburgerIcon />}
-          onClick={() => changeDisplay("flex")}
-          display={["flex", "flex", "none", "none"]}
+          aria-label="Open Menu" size="lg" mr={2} icon={<HamburgerIcon />} onClick={() => changeDisplay("flex")} display={["flex", "flex", "none", "none"]}
         />
       </Flex>
 
       {/* Mobile Content */}
       <Flex
-        w="100vw"
-        display={display}
-        bgColor="gray.200"
-        zIndex={20}
-        h="100vh"
-        pos="fixed"
-        top="0"
-        left="0"
-        py={10}
-        px={5}
-        overflowY="auto"
-        flexDir="column"
+        w="375px" h="812px" top="0px" left="0px" display={display} bgColor="rgba(255, 193, 118, 0.9)" backdropFilter="blur(25px)" zIndex={20} pos="absolute" py={10} px={5} overflowY="auto" flexDir="column"
       >
         <Flex justify="flex-end">
           <IconButton
-            mt={2}
-            mr={2}
-            aria-label="Open Menu"
-            size="lg"
-            icon={<CloseIcon />}
-            onClick={() => changeDisplay("none")}
+            mt={2} mr={2} aria-label="Open Menu" size="lg" icon={<CloseIcon />} onClick={() => changeDisplay("none")}
           />
         </Flex>
 
@@ -130,29 +81,18 @@ const Navbar = () => {
           {isLogin ? (
             <Menu>
               <MenuButton
-                as={Button}
-                rightIcon={<ChevronDownIcon />}
-                variant="ghost"
-                cursor="pointer"
-                p={0}
-                fontSize="md"
+                as={Button} rightIcon={<ChevronDownIcon />} variant="ghost" cursor="pointer" p={0} fontSize="md"
               >
                 Lorem Ipsum
               </MenuButton>
-              <MenuList>
+              <MenuList bgColor="#FFFFFF" border="2px solid #FFA06F" borderRadius="10px">
                 <MenuItem>Dashboard</MenuItem>
                 <MenuItem onClick={() => setIsLogin(false)}>Logout</MenuItem>
               </MenuList>
             </Menu>
           ) : (
             <Button
-              boxShadow="md"
-              h="30px"
-              fontSize="15px"
-              px={8}
-              color="white"
-              colorScheme="yellow"
-              w={{ md: "100%", base: "35%" }}
+              boxShadow="0px 1px 7px 2px rgba(244, 166, 65, 0.62)" borderRadius="10px" fontSize="15px" h="30px" px={8} color="white" bgColor="#F4A641" w={{ md: "100%", base: "35%" }}
               onClick={() => setIsLogin(true)}
             >
               Log In
