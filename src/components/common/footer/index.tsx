d58e96932a0b030f7a69aa8eaa58a4c020152b63
msyahrulsp <mssp892@gmail.com>
2022-08-08 10:55:23 +0700

    refactor: Remove md (dah ada base)

--- src/components/common/footer/index.tsx
+++ src/components/common/footer/index.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
 import { Box, Center, Text, Circle, VStack, HStack} from '@chakra-ui/react';
 
 const Footer = () => {
+  // TODO: Ubah kalau dah ada content footernya
   return (
     <Box
       bg='#ffa06e'
-      p={{ base: '32px', md: '32px', lg: '50px' }}
+      px={4}
+      py={6}
+      mt={5}
     >
-      <VStack spacing={{ base: '0px', md: '0px', lg: '43px' }} mt={{ base: '0px', md: '0px', lg: '20px' }}>
-        <HStack spacing={{ base: '17px', md: '17px', lg: '30px' }}>
-          <Circle size={{ base: '91px', md: '91px', lg: '186px' }} bg='#D9D9D9'></Circle>
+      <VStack spacing={{ base: '0px', lg: '20px' }} mt={{ base: '0px', lg: '20px' }}>
+        <HStack spacing={{ base: '17px', lg: '30px' }}>
+          <Circle size={{ base: '91px', lg: '186px' }} bg='#D9D9D9'></Circle>
           <Box
             fontFamily='Alegreya Semibold'
-            fontSize={{ base: '36px', md: '36px', lg: '73px' }}
-            lineHeight={{ base: '33.12px', md: '33.12px', lg: '67.16px' }}
+            fontSize={{ base: '36px', lg: '73px' }}
+            lineHeight={{ base: '33.12px', lg: '67.16px' }}
           >
             <Text>KAT</Text>
             <Text>ITB</Text>
@@ -23,8 +25,8 @@ const Footer = () => {
         <Center bg='#D9D9D9' h={[209, 209, 229]} w={[0, 0, 0, 1320]}>
           <Text
             fontFamily='Alegreya Semibold'
-            fontSize={{ base: '0px', md: '0px', lg: '73px' }}
-            lineHeight={{ base: '33.12px', md: '33.12px', lg: '67.16px' }}
+            fontSize={{ base: '0px', lg: '73px' }}
+            lineHeight={{ base: '33.12px', lg: '67.16px' }}
           >
             Footer Content
           </Text>
