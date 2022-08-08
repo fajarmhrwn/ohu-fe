a0264391c473a8c968619ccaae61fd6198194821
joli <auliameydivaannandya@Aulias-MacBook-Pro.local>
2022-08-08 11:44:37 +0700

    tembahan responsive

--- src/components/common/Event/index.tsx
+++ src/components/common/Event/index.tsx
@@ -5,79 +5,75 @@ import { Box,
         Image,
         Flex,
         Stack,
-        Button,
-        useMediaQuery,
-        Show,
-        Hide } from '@chakra-ui/react'
+        Button } from '@chakra-ui/react'
 
         
 export const Event: () => JSX.Element = () => {
 
     return (
             <Box backgroundColor='#FFEBB0'
-            w={{lg: '1920px', base: '640px'}}
-            h='1200'>
+            w={{lg: 'auto', base: '640px'}}
+            h='3600px'>
             <Flex>
-            <Heading fontSize={{lg: '124px', base:'70px'}}
+            <Heading fontSize={{'2xl': '240px',lg: '124px', base:'70px'}}
                 pos='relative'
-                marginLeft={{lg: '1430px', base:'300px'}} 
-                marginRight='auto'
-                top={{lg: '200px', base:'400px'}}
-                w='370px'
-                h='136px'>
-            Events
+                marginLeft={{lg: '75%', base:'330px'}} 
+                marginRight='10%'
+                top={{lg: '150px', base:'370px'}}
+                w='70%px'>
+                Events
             </Heading> 
             </Flex>
                     <Text fontSize='43px'
                         display={{lg: 'block', base:'none'}} 
-                        pos='absolute' 
-                        left='170px' 
-                        top='360px' 
+                        pos='relative' 
+                        left='10%' 
+                        top='3%' 
                         w='855px' 
                         h='40px'
                         >
                         Apa saja yang dapat kita lakukan di OHU Event?
                     </Text>
                     <Image pos='absolute'
-                        display={{lg: 'block', base:'none'}} 
-                        top='500px' 
+                        display={{'2xl': 'block', lg: 'block', base:'none'}} 
+                        top='15%' 
                         marginRight='auto' 
-                        marginLeft='80px' 
-                        w='700px' 
+                        marginLeft='5%' 
+                        w='40%' 
                         src='https://i.postimg.cc/NGPR6LNw/Group-5479.png'/>
             <Flex>
                 <Stack direction='column' 
                     pos='relative'
                     top={{lg: '450px', base:'600px'}}
-                    left={{lg: '830px', base:'100px'}}
+                    left={{lg: '45%', base:'20%'}}
                     spacing = {8}>
                     <Button backgroundColor='#FFA06F' 
-                        w='400px' 
-                        h='70px' 
-                        borderRadius='50px'
-                        fontSize='35px'>
+                        w='150%' 
+                        h='90%'
+                        borderRadius='50px' 
+                        fontSize='200%'>
                         OHU Map Tour
                     </Button>
                     <Button backgroundColor='#FFA06F' 
-                        w='400px' 
-                        h='70px' 
+                        w='150%' 
+                        h='90%'
                         borderRadius='50px' 
-                        fontSize='35px'>
+                        fontSize='200%'>
                         Missions
-                    </Button>
+                    </Button>  
                     <Button backgroundColor='#FFA06F' 
-                        w='400px' 
-                        h='70px' 
+                        w='150%' 
+                        h='90%'
                         borderRadius='50px' 
-                        fontSize='35px'>
+                        fontSize='200%'>
                         Leaderboards
                     </Button>  
                 </Stack>
                 <Image pos='absolute' 
-                    w={{lg: '454px', base:'300px'}} 
-                    top={{lg: '500px', base:'450px'}} 
+                    w={{lg: '30%', base:'300px'}} 
+                    top={{lg: '15%', base:'420px'}} 
                     marginRight='auto' 
-                    marginLeft={{lg: '1320px', base:'150px'}}
+                    marginLeft={{lg: '68%', base:'180px'}}
                     src='https://i.postimg.cc/W3N0nZLs/21-22-joget-2-1.png'/>
             </Flex>
             </Box>
