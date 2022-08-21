c79f43c81105f77351ebff75cedb45840c9ae913
dyophine7 <16521248@mahasiswa.itb.ac.id>
2022-08-21 17:17:21 +0700

    feat: add background music

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,55 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import { Loading } from '@components/common/Loading';
 import { Routing } from './routing';
+import ReactAudioPlayer from 'react-audio-player';
+import BackgroundMusic from './assets/background-music.mp3';
 
-const App = () => (
-  <BrowserRouter>
-    <AnimatePresence exitBeforeEnter>
-      <Routes>
-        {Routing.map((route) => {
-          const Component = route.component;
-          return (
-            <Route
-              caseSensitive
-              path={route.path}
-              key={route.path}
-              element={
-                <Suspense fallback={<Loading />}>
-                  <Component />
-                </Suspense>
-              }
-            />
-          );
-        })}
-      </Routes>
-    </AnimatePresence>
-  </BrowserRouter>
-);
+function fixComponent<T>(component: T): T {
+  return (component as any).default ?? component;
+}
+const ReactAudioPlayerComponent = fixComponent(ReactAudioPlayer);
+
+const App = () => {
+  const musicTrigger = () => {
+    const music = document.getElementById(
+      'backgroundMusic'
+    ) as HTMLAudioElement;
+    if (music != null) {
+      music.play();
+    }
+  };
+
+  return (
+    <div onClick={musicTrigger}>
+      <BrowserRouter>
+        <AnimatePresence exitBeforeEnter>
+          <Routes>
+            {Routing.map((route) => {
+              const Component = route.component;
+              return (
+                <Route
+                  caseSensitive
+                  path={route.path}
+                  key={route.path}
+                  element={
+                    <Suspense fallback={<Loading />}>
+                      <Component />
+                    </Suspense>
+                  }
+                />
+              );
+            })}
+          </Routes>
+        </AnimatePresence>
+      </BrowserRouter>
+      <ReactAudioPlayerComponent
+        id="backgroundMusic"
+        src={BackgroundMusic}
+        autoPlay={true}
+        loop
+      />
+    </div>
+  );
+};
 
 export default App;
