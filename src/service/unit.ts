45c56713f463cb43bc4dde5ddf22c2e97fd9da4d
rayhankinan <rayhankinan@gmail.com>
2022-08-27 01:22:21 +0700

    Hapus header

--- src/service/unit.ts
+++ src/service/unit.ts
@@ -3,37 +3,18 @@ import axios from 'axios';
 const BASE_URL: string = import.meta.env.VITE_API_URL;
 
 export const getUnits = async (url: string) => {
-  const { data } = await axios.get(`${BASE_URL}${url}`, {
-    headers: {
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjYxNTM3NDA5LCJleHAiOjE2NjQxMjk0MDl9.q2suJaCPESIAzav-6jPYxHmMI2CQTmFi5OFE899UyIQ'
-    }
-  });
+  const { data } = await axios.get(`${BASE_URL}${url}`);
 
   return data;
 };
 
 export const getUnitById = async (id: string) => {
-  const { data } = await axios.get(`${BASE_URL}/units/${id}`, {
-    headers: {
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjYxNTM3NDA5LCJleHAiOjE2NjQxMjk0MDl9.q2suJaCPESIAzav-6jPYxHmMI2CQTmFi5OFE899UyIQ'
-    }
-  });
+  const { data } = await axios.get(`${BASE_URL}/units/${id}`);
   return data;
 };
 
 export const updateVisitors = async (id: string, visitors: number) => {
-  await axios.put(
-    `${BASE_URL}/units/${id}`,
-    { visitors },
-    {
-      headers: {
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjYxNTM3NDA5LCJleHAiOjE2NjQxMjk0MDl9.q2suJaCPESIAzav-6jPYxHmMI2CQTmFi5OFE899UyIQ'
-      }
-    }
-  );
+  await axios.put(`${BASE_URL}/units/${id}`, { visitors });
 };
 
 export const getScore = async (url: string) => {
